ffc8f2db9efcf6927313ac24e52def7e9c2b2fba
Matthew-Jones-CS <mjone355@charlotte.edu>
2025-04-26 19:04:21 -0400

    Spelling error and a new js feature
    What is said in the title.

---
+++ itis3135/projects/scripts/input_encodeing.js
@@ -0,0 +1,24 @@
+const inputObj = document.getElementById(".name");
+const submitButton = document.getElementById(".submit");
+
+function encode()
+{
+    console.log("hi");
+    const dangerCharacters = ["<", ">", "\"", "\\", "\`", "\&", ".", "\$"];
+    let temp = "";
+    for (character of inputObj.innerText)
+    {
+        if (dangerCharacters.indexOf(character) >= 0)
+        {
+            temp += "%" + dangerCharacters.indexOf(character);
+        }
+        else
+        {
+            temp += character;
+        }
+    }
+    const encodedInput = temp;
+    console.log(encodedInput);
+    return encodedInput;
+}
+submitButton.addEventListener("click", encode);
